0395e2a977dcd3b1815d6a756e6402b39c127584
nilotpaul <nilotpaul.nandi@gmail.com>
2023-03-25 19:28:46 +0530

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Array from './components/Array';
+import Homepage from './components/Homepage';
+import Header from './components/Header';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import String from './components/String';
+import Searchsort from './components/Searchsort';
+import Stackqueue from './components/Stackqueue';
+import Matrix from './components/Matrix';
+import Linkedlist from './components/Linkedlist';
+import "./App.css"
+// import { Button } from '@material-ui/core';
+
+// const theme = createTheme({
+//   palette: {
+//     mode: "dark",
+//   },
+// }); not required the dark theme anymore
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+      
+      
+      <div className='App'>
+        
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+        <Route exact path= "/" element={<Homepage/>}/>
+        <Route path= "/array" element={<Array/>}/>
+        <Route path='/string' element={<String/>}/>
+        <Route path='/linkedlist' element={<Linkedlist/>}/>
+        <Route path='/matrix' element={<Matrix/>}/>
+        <Route path='/searchsort' element={<Searchsort/>}/>
+        <Route path='/stackqueue' element={<Stackqueue/>}/>
+        </Routes>
+      </BrowserRouter>
+      </div>
+      
+
+   );
 }
 
 export default App;
